03efb4dddb80950b9f4c588c8169381566749843
Erfan <erfanhanifezade@gmail.com>
2022-01-11 21:28:40 +0330

    complete styles

--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./post";
+// import { FileUploaderClient } from "../proto/app_grpc_web_pb";
 
 const Posts = () => {
   let posts = useState([]);
   let currentPage = useState(1);
   let postsPerPage = useState(10);
 
+  // let client = new FileUploaderClient("localhost:5000");
+
+  // useEffect(() => {
+
+  // }, [currentPage])
+
   return (
-    <section className="w-full min-h-forMain h-auto flex flex-wrap justify-center align-start space-x-2 pt-5 gap-4">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <section className="w-full">
+      <div className="min-h-forMain h-auto flex flex-wrap justify-center align-start space-x-2 pt-5 gap-4">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+      <div className="w-full mt-5 flex justify-around align-center p-4">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Last
+        </button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Next
+        </button>
+      </div>
     </section>
   );
 };
